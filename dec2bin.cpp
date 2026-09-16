8cc380702444696210a3ccdd3e18db78ec91e852
AnshulPrajapati695 <parjapatianshul695@gmail.com>
2026-09-16 10:10:58 +0530

    add some optimized code

--- dec2bin.cpp
+++ dec2bin.cpp
@@ -5,13 +5,13 @@ int main(){
     int num;
     cout<<"Enter a decimal number : ";
     cin>>num;
-    int r;
     int binary=0;
-    while (num>0){
-        r = num%2;
+    int pow = 1; //10^0
+    while (num>=0){
+        int r = num%2;
         num /= 2;
-        binary += r;
-        binary *= 10;
+        binary += r*pow;
+        pow *= 10;
 
     }
     cout<<"The binary number is "<<binary;
