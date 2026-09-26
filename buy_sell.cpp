62743b975560a181672ca2838329e0dbbbbeac35
AnshulPrajapati695 <parjapatianshul695@gmail.com>
2026-09-26 17:58:06 +0530

    Leetcode Problem no. - 11

---
+++ buy_sell.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mp = 0;
+        int bestbuy = prices[0];
+        for(int i=1;i<prices.size();i++){
+            if(bestbuy < prices[i]){
+                mp = max(mp,prices[i]-bestbuy);
+            }
+            bestbuy = min(bestbuy,prices[i]);
+        }
+        return mp;
+    }
+};
